87cc59b7b45862b1b1964146726b83bd22325df8
LimHoSung <wonder789@naver.com>
2017-12-25 22:35:03 +0900

    App Title 변경

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App title="Todo App"/>, document.getElementById('root'));
+ReactDOM.render(<App title="할 일 목록"/>, document.getElementById('root'));
 registerServiceWorker();
