fc8d730e38d043055b7a4c6f092e59670099e507
LimHoSung <wonder789@naver.com>
2017-12-25 22:35:20 +0900

    Todo 상태 & filter 그룹 추가

---
+++ src/components/TodoFooter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import CheckBox from './CheckBox';
+
+const todoFooter = ( props ) => {
+
+    const unCompleteList = props.todoList.filter( todo => {
+        return !todo.complete;
+    });
+
+    return (
+        <div className="TodoFooter">
+            <div className="TodoStatus">
+                <span>남은 할 일 {unCompleteList.length} 개</span>
+            </div>
+            <div className="TodoFilterGroup">
+                {
+                    props.filterGroup.map( (obj, index) => {
+                        return <CheckBox
+                                     clicked={ () => props.todoFilter(obj.type) }
+                                     checked={ props.filterType === obj.type }
+                                     label={ obj.name }
+                                />;
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default todoFooter;
