21b2e4bb3c176afb462b19bae2d741f0d5eebe0d
LimHoSung <wonder789@naver.com>
2017-12-25 22:35:10 +0900

    컴포넌트 추가

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import TodoHeader from './TodoHeader';
+import TodoBody from './TodoBody';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
+import TodoFooter from './TodoFooter';
+import CheckBox from './CheckBox';
 
-export { TodoHeader, TodoForm, TodoItem };
+export { TodoHeader, TodoForm, TodoItem, TodoFooter, TodoBody };
